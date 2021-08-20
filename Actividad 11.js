cc565fd12d35765c6178437ffe1961ea6eb86919
JD-Espinoza <84589218+JD-Espinoza@users.noreply.github.com>
2021-08-20 14:01:14 -0700

    Curso HTML

---
+++ Actividad 11.js
@@ -0,0 +1,6 @@
+//function load() {}
+//load = function(){ }
+load = () => {
+    //document.getElementByID("frmsignin").addeventlistener("submit",frmsubmit)
+frmSignIn.addEventListener("submit",frmSignIn);
+}
