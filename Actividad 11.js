534f702d402267ea07dc233e021404d0adab68aa
JD-Espinoza <84589218+JD-Espinoza@users.noreply.github.com>
2021-08-20 14:06:07 -0700

    Delete Actividad 11.js

--- Actividad 11.js
+++
@@ -1,6 +0,0 @@
-//function load() {}
-//load = function(){ }
-load = () => {
-    //document.getElementByID("frmsignin").addeventlistener("submit",frmsubmit)
-frmSignIn.addEventListener("submit",frmSignIn);
-}
